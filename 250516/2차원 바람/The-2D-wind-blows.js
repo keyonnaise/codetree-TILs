d58aeffdec2ee19f9fb24f25dde14f0cbc98f6f7
Jisu Park <55680464+keyonnaise@users.noreply.github.com>
2025-05-16 00:39:24 +0900

    Title: 2차원 바람, Time: 291ms, Memory: 22MB, Status: Passed - Codetree

--- 250516/2차원 바람/The-2D-wind-blows.js
+++ 250516/2차원 바람/The-2D-wind-blows.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m, q] = input[0].split(" ").map(Number);
-const grid = input.slice(1, 1 + n).map(line => line.split(" ").map(Number));
+const grid = input.slice(1, 1 + n).map(line => line.trim().split(" ").map(Number));
 const winds = input.slice(1 + n, 1 + n + q).map(line => line.split(" ").map(Number));
 
 // Please Write your code here.
